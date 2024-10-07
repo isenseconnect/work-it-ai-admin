be823aedaaae761e9d17f9b0e773719576f46316
jasdeep <jasdeep.saggu@fluidai.com>
2024-10-07 20:21:24 +0530

    backend url update config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  baseURL: "http://localhost:4000/api/",
+  baseURL: "https://diditwithai-api-dev.neoterichs.com/api/",
   // baseURL: 'https://workitai-backend.neoterichs.com/api/',
   SCREENSHOT_SERVICE_URL: "http://localhost:3100/api/",
 };
